8ccf81f7cbc2be98dc4e4bd4fbc501e0533146f5
garciadavid2000 <garciadavid2003@gmail.com>
2025-11-15 21:18:53 -0500

    Add database config to app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db
 
-app = Flask(__name__)  
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///streaks.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # for dev; in prod you'd use migrations
+
+    return app
+
+app = create_app()
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
